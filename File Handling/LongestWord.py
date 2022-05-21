dbed97da2faf290fd3c7072b81f3e7c8d172ebe9
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-05-21 22:21:46 +0500

    Practice exercises on file handling

---
+++ File Handling/LongestWord.py
@@ -0,0 +1,16 @@
+# Write a python program to find the longest word from the file
+def LongestWord(filename):
+    # opening of  filename
+    with open(filename) as f:
+        data = f.read().split(' ')
+        word = len(max(data,key=len))
+        for i in data:
+            if len(i)==word:
+                print("Longest word found in file is : " + i)
+        
+filename =input("Enter name of the file, from where you want to find the longest word from the file : ")
+LongestWord(filename)
+    
+    
+    
+
