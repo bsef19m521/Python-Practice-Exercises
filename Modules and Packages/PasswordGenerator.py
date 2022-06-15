87f1f36cdefad7014e7645ffb69e1e1a981ba87d
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-06-15 21:01:12 +0500

    Practice exercises on module and packages

--- Modules and Packages/PasswordGenerator.py
+++ Modules and Packages/PasswordGenerator.py
@@ -2,10 +2,14 @@ import random
 
 # list of all the punctutaion signs 
 punctutaionSigns = ['!','@','#','$','%','^','&','*','(',')']
-# two random upper case letters
+# two random generated upper case letters
 upperCaseLetters = chr(random.randint(65,90)) + chr(random.randint(60,90))
+# two random generated lower case letters
 lowerCaseLetters = chr(random.randint(97,122)) + chr(random.randint(97,122))
+# two random generated integer numbers
+# str function is used convert integer generated numbers into a string
 digits = str(random.randint(0,9)) + str(random.randint(0,9))
+
 specialCharacters = random.choice(punctutaionSigns) + random.choice(punctutaionSigns)
 password = list(upperCaseLetters+lowerCaseLetters+digits+specialCharacters)
 random.shuffle(password)
