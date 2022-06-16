a176a8d34ea56cea1c105bebfa163f9e63712cf9
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-06-16 14:18:24 +0500

    Practice exercises on OOP using Python

--- Object Oriented Programming using Python/Tossing_Coin.py
+++ Object Oriented Programming using Python/Tossing_Coin.py
@@ -2,16 +2,16 @@
 import random
 class Coin:
     def __init__(self):
-        self.sideup = 'Head'
+        self.__sideup = 'Head'
         
     def toss(self):
         if random.randint(0,1)==0:
-            self.sideup = 'Head'
+            self.__sideup = 'Head'
         else:
-            self.sideup = 'Tail'
+            self.__sideup = 'Tail'
             
     def get_sideup(self):
-        return self.sideup
+        return self.__sideup
 def main():
     # creating object of the class 
     obj1 = Coin()
@@ -19,10 +19,11 @@ def main():
     print("Up side of the coin is : " + str(obj1.get_sideup()))
     
     # tossing of the Coin 
-    print("I'm tossing a coin......")
-    obj1.toss()
+    print("I'm tossing a coin ten times! ")
+    for i in range(0,10):
+        obj1.toss()
+            # After tossing the up side of the coin is 
+        print("Up side of the coin is : " + str(obj1.get_sideup()))
     
-    # After tossing the up side of the coin is 
-    print("After tossing the up side of the coin : " + str(obj1.get_sideup()))
     
 main()
