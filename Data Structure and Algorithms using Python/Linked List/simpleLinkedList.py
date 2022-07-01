ec607737f68f2931cc366b93b4ebdb6aae3e72fd
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-07-02 04:04:22 +0500

    Data structure and algorithms using Python

--- Data Structure and Algorithms using Python/Linked List/simpleLinkedList.py
+++ Data Structure and Algorithms using Python/Linked List/simpleLinkedList.py
@@ -10,6 +10,14 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None # function to initialize the head of the linkedList
+    
+    # this function prints content of linked list started from the head of the linkedList
+    def displayList(self):
+        current = self.head
+        print("All the data in the linkedList is available : ")
+        while current != None:
+            print(current.data)
+            current = current.next
         
 if __name__ == '__main__':
     # start with any empty linkedList
@@ -31,3 +39,8 @@ if __name__ == '__main__':
     print("First Node of the linkedList: " + str(list1.head.data))
     print("Second Node of the linkedList: " + str(second.data))
     print("Third Node of the linkedList: " + str(third.data))
+    
+    list1.displayList()
+    # print(list1.head.next)
+    # print(second.next)
+    # print(third.next)
