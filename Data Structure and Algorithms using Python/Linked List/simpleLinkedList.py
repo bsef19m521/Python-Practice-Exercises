09173dda7f1de599aabd308f81cad664115882cf
bsef19m521 <bsef19m521@pucit.edu.pk>
2022-07-02 03:48:24 +0500

    Data structure and algorithms using Python

---
+++ Data Structure and Algorithms using Python/Linked List/simpleLinkedList.py
@@ -0,0 +1,12 @@
+# create a node class 
+class Node:
+    # function to initalize node object 
+    def __init__(self):
+        self.data = data    #this line assign data to the node
+        self.next = None    #next/reference to the Node is null
+
+# LinkedList is a collection of multiple nodes
+# This class also contains the object of the node class  
+class LinkedList:
+    def def __init__(self):
+        self.head = None # function to initialize the head of the linkedList
--- File Handling/WordsFrequency.py
+++ File Handling/WordsFrequency.py
@@ -1,2 +1,3 @@
 # Write a Python program to count the frequency of words in a file.
+string= "Hello, world!, Python programming language is used for machine learning, data science and web development"
 
